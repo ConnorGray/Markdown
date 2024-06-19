e6e46498aaaba49c2dfd1d18a197b3be05876fd5
Connor Gray <code@connorgray.com>
2024-06-19 12:47:58 -0500

    cleanup(markdown-ast): Factor to/from events logic out into dedicated files

---
+++ crates/markdown-ast/src/from_events.rs
@@ -0,0 +1,346 @@
+//! Convert from "unflattened" [`pulldown_cmark::Event`]s to AST [`Block`]s.
+
+use pulldown_cmark::{Event, Tag};
+
+use std::mem;
+
+use crate::{
+    unflatten::UnflattenedEvent, Block, CodeBlockKind, Inline, Inlines,
+    ListItem,
+};
+
+//======================================
+// AST Builder
+//======================================
+
+pub(crate) fn ast_events_to_ast(events: Vec<UnflattenedEvent>) -> Vec<Block> {
+    let mut complete: Vec<Block> = vec![];
+
+    let mut text_spans: Vec<Inline> = vec![];
+
+    for event in events {
+        // println!("event: {:?}", event);
+
+        if !is_inline(&event) {
+            if !text_spans.is_empty() {
+                complete.push(Block::Paragraph(Inlines(mem::replace(
+                    &mut text_spans,
+                    vec![],
+                ))));
+            }
+        }
+
+        match event {
+            UnflattenedEvent::Event(event) => match event {
+                Event::Start(_) | Event::End(_) => {
+                    panic!("illegal Event::{{Start, End}} in UnflattenedEvent::Event")
+                },
+                Event::Text(text) => {
+                    text_spans.push(Inline::Text(text.to_string()))
+                },
+                Event::Code(code) => {
+                    text_spans.push(Inline::Code(code.to_string()))
+                },
+                Event::SoftBreak => text_spans.push(Inline::SoftBreak),
+                Event::HardBreak => text_spans.push(Inline::HardBreak),
+                Event::Html(_) => todo!("error: unhandled inline HTML"),
+                Event::InlineHtml(_) => todo!(),
+                Event::Rule => complete.push(Block::Rule),
+                Event::TaskListMarker(_) | Event::FootnoteReference(_) => {
+                    todo!("handle: {event:?}")
+                },
+                Event::InlineMath(_) => todo!(),
+                Event::DisplayMath(_) => todo!(),
+            },
+            UnflattenedEvent::Nested { tag, events } => {
+                match tag {
+                    //
+                    // Inline content
+                    //
+                    Tag::Emphasis => {
+                        text_spans.push(Inline::Emphasis(unwrap_text(events)));
+                    },
+                    Tag::Strong => {
+                        text_spans.push(Inline::Strong(unwrap_text(events)));
+                    },
+                    Tag::Strikethrough => {
+                        text_spans
+                            .push(Inline::Strikethrough(unwrap_text(events)));
+                    },
+
+                    Tag::Link {
+                        link_type,
+                        dest_url,
+                        title,
+                        id,
+                    } => {
+                        let content_text = unwrap_text(events);
+
+                        text_spans.push(Inline::Link {
+                            link_type,
+                            dest_url: dest_url.to_string(),
+                            title: title.to_string(),
+                            id: id.to_string(),
+                            content_text,
+                        })
+                    },
+
+                    //
+                    // Block content
+                    //
+
+                    // TODO: Use the two Heading fields that are ignored here?
+                    Tag::Heading {
+                        level,
+                        id: _,
+                        classes: _,
+                        attrs: _,
+                    } => {
+                        complete
+                            .push(Block::Heading(level, unwrap_text(events)));
+                    },
+                    // TODO(test):
+                    //     Is this disappearance of the Paragraph tag correct?
+                    Tag::Paragraph => text_spans.extend(unwrap_text(events)),
+                    // TODO: Include the list start number in the metadata
+                    Tag::List(_start) => {
+                        let mut items: Vec<ListItem> = Vec::new();
+
+                        for event in events {
+                            if let UnflattenedEvent::Nested {
+                                tag: Tag::Item,
+                                events: item_events,
+                            } = event
+                            {
+                                let item_blocks =
+                                    ast_events_to_ast(item_events);
+                                items.push(ListItem(item_blocks));
+                            } else {
+                                todo!("handle list element: {event:?}");
+                            }
+                        }
+
+                        complete.push(Block::List(items));
+                    },
+                    Tag::Item => {
+                        complete.extend(ast_events_to_ast(events));
+                    },
+                    Tag::CodeBlock(kind) => {
+                        let text_spans = unwrap_text(events);
+                        let code_text = text_to_string(&text_spans);
+
+                        let kind = CodeBlockKind::from_pulldown_cmark(kind);
+
+                        complete.push(Block::CodeBlock {
+                            kind,
+                            code: code_text,
+                        })
+                    },
+                    Tag::BlockQuote(kind) => {
+                        let blocks = ast_events_to_ast(events);
+                        complete.push(Block::BlockQuote { kind, blocks })
+                    },
+                    Tag::Table(alignments) => {
+                        let mut events = events.into_iter();
+                        let header_events = match events.next().unwrap() {
+                            UnflattenedEvent::Event(_) => panic!(),
+                            UnflattenedEvent::Nested { tag, events } => {
+                                assert!(tag == Tag::TableHead);
+                                events
+                            },
+                        };
+
+                        let mut headers = Vec::new();
+
+                        for table_cell in header_events {
+                            let table_cell_text =
+                                unwrap_text(unwrap_table_cell(table_cell));
+
+                            headers.push(table_cell_text);
+                        }
+
+                        let mut rows = Vec::new();
+
+                        for row_events in events {
+                            let row_events = match row_events {
+                                UnflattenedEvent::Event(_) => panic!(),
+                                UnflattenedEvent::Nested { tag, events } => {
+                                    assert!(tag == Tag::TableRow);
+                                    events
+                                },
+                            };
+
+                            let mut row = Vec::new();
+
+                            for table_cell in row_events {
+                                let table_cell_text =
+                                    unwrap_text(unwrap_table_cell(table_cell));
+
+                                row.push(table_cell_text);
+                            }
+
+                            rows.push(row);
+                        }
+
+                        complete.push(Block::Table {
+                            alignments,
+                            headers,
+                            rows,
+                        })
+                    },
+                    _ => todo!("handle: {tag:?}"),
+                }
+            },
+        }
+    }
+
+    if !text_spans.is_empty() {
+        complete.push(Block::paragraph(text_spans));
+    }
+
+    complete
+}
+
+/// Returns `true` if `event` contains content that can be added "inline" with text
+/// content.
+///
+/// `event`'s that cannot be added inline will start a new [`Block`].
+fn is_inline(event: &UnflattenedEvent) -> bool {
+    match event {
+        UnflattenedEvent::Event(event) => match event {
+            Event::Start(_) | Event::End(_) => unreachable!(),
+            Event::Text(_) => true,
+            Event::Code(_) => true,
+            Event::SoftBreak => true,
+            Event::HardBreak => true,
+            // TODO: HTML could cause break to next block?
+            Event::Html(_) => false,
+            Event::InlineHtml(_) => todo!(),
+            Event::Rule => false,
+            Event::TaskListMarker(_) => false,
+            Event::FootnoteReference(_) => true,
+            Event::InlineMath(_) => todo!(),
+            Event::DisplayMath(_) => todo!(),
+        },
+        UnflattenedEvent::Nested { tag, events: _ } => match tag {
+            Tag::Emphasis | Tag::Strong | Tag::Strikethrough => true,
+            Tag::Heading {
+                level: _,
+                id: _,
+                classes: _,
+                attrs: _,
+            } => false,
+            Tag::Paragraph => false,
+            Tag::List(_) => false,
+            Tag::Item => false,
+            Tag::CodeBlock(_) => false,
+            Tag::BlockQuote(_kind) => false,
+            Tag::Table(_) => false,
+            Tag::TableHead | Tag::TableRow => unreachable!(),
+            Tag::Link { .. } => true,
+            _ => todo!("handle tag: {tag:?}"),
+        },
+    }
+}
+
+fn unwrap_text(events: Vec<UnflattenedEvent>) -> Inlines {
+    let mut text_spans: Vec<Inline> = vec![];
+
+    for event in events {
+        match event {
+            UnflattenedEvent::Event(event) => match event {
+                Event::Start(_) | Event::End(_) => unreachable!(),
+                Event::Text(text) => {
+                    text_spans.push(Inline::Text(text.to_string()))
+                },
+                Event::Code(code) => {
+                    text_spans.push(Inline::Code(code.to_string()))
+                },
+                Event::SoftBreak => text_spans.push(Inline::SoftBreak),
+                Event::HardBreak => text_spans.push(Inline::HardBreak),
+                Event::Html(_) => todo!("error: skipping inline HTML"),
+                Event::InlineHtml(_) => todo!(),
+                Event::TaskListMarker(_)
+                | Event::Rule
+                | Event::FootnoteReference(_) => {
+                    todo!("handle: {event:?}")
+                },
+                Event::InlineMath(_) => todo!(),
+                Event::DisplayMath(_) => todo!(),
+            },
+            UnflattenedEvent::Nested { tag, events } => match tag {
+                Tag::Emphasis => {
+                    text_spans.push(Inline::Emphasis(unwrap_text(events)));
+                },
+                Tag::Strong => {
+                    text_spans.push(Inline::Strong(unwrap_text(events)));
+                },
+                Tag::Strikethrough => {
+                    text_spans.push(Inline::Strikethrough(unwrap_text(events)));
+                },
+                Tag::Paragraph => {
+                    // If this is a separate paragraph, insert two hardbreaks
+                    // (two newlines). Don't insert hardbreaks if there isn't any existing
+                    // text content, to avoid having leading empty lines.
+                    if !text_spans.is_empty() {
+                        // TODO: Replace this with a new Inline::ParagraphBreak?
+                        //       A HardBreak is just a newline.
+                        text_spans.push(Inline::HardBreak);
+                        text_spans.push(Inline::HardBreak);
+                    }
+                    text_spans.extend(unwrap_text(events))
+                },
+                Tag::Link {
+                    link_type,
+                    dest_url,
+                    title,
+                    id,
+                } => {
+                    let content_text = unwrap_text(events);
+
+                    text_spans.push(Inline::Link {
+                        link_type,
+                        dest_url: dest_url.to_string(),
+                        title: title.to_string(),
+                        id: id.to_string(),
+                        content_text,
+                    })
+                },
+                _ => todo!("handle {tag:?}"),
+            },
+        }
+    }
+
+    Inlines(text_spans)
+}
+
+fn unwrap_table_cell(event: UnflattenedEvent) -> Vec<UnflattenedEvent> {
+    match event {
+        UnflattenedEvent::Event(_) => panic!(),
+        UnflattenedEvent::Nested { tag, events } => {
+            assert_eq!(tag, Tag::TableCell, "expected to get Tag::TableCell");
+            events
+        },
+    }
+}
+
+fn text_to_string(Inlines(text_spans): &Inlines) -> String {
+    let mut string = String::new();
+
+    for span in text_spans {
+        match span {
+            Inline::Text(text) => {
+                string.push_str(&text);
+            },
+            Inline::SoftBreak => {
+                string.push_str(" ");
+            },
+            Inline::HardBreak => {
+                string.push_str("\n");
+            },
+            _ => todo!("handle span: {span:?}"),
+        }
+    }
+
+    string
+}
---
+++ crates/markdown-ast/src/to_events.rs
@@ -0,0 +1,180 @@
+//! Convert from AST [`Block`]s to "flattened" [`pulldown_cmark::Event`]s.
+
+use pulldown_cmark::{CowStr, Event, Tag};
+
+use crate::{Block, Inline, Inlines, ListItem};
+
+//======================================
+// AST blocks to Events
+//======================================
+
+pub(crate) fn block_to_events<'ast>(
+    block: &'ast Block,
+    events: &mut Vec<Event<'ast>>,
+) {
+    match block {
+        Block::Paragraph(inlines) => wrap(Tag::Paragraph, events, |events| {
+            inlines_to_events(inlines, events)
+        }),
+        Block::List(list_items) => {
+            // TODO: Handle this for numbered lists.
+            let first_item_number = None;
+
+            wrap(Tag::List(first_item_number), events, |events| {
+                for ListItem(list_item_blocks) in list_items {
+                    wrap(Tag::Item, events, |events| {
+                        // NOTE:
+                        //  Handle a special case where a single-item list
+                        //  containing a sequence of inlines is parsed by
+                        //  clap-markdown NOT wrapped in paired
+                        //  Start(Tag::Paragraph) / End(_) events.
+                        match list_item_blocks.as_slice() {
+                            [Block::Paragraph(inlines)]
+                                if list_items.len() == 1 =>
+                            {
+                                inlines_to_events(inlines, events);
+
+                                // Return from inner closure.
+                                return;
+                            },
+                            _ => (),
+                        }
+
+                        for list_item_block in list_item_blocks {
+                            block_to_events(list_item_block, events);
+                        }
+                    });
+                }
+            })
+        },
+        Block::Heading(level, inlines) => {
+            let tag = Tag::Heading {
+                level: *level,
+                // FIXME: Set this id.
+                id: None,
+                // FIXME: Support these classes and attrs.
+                classes: Vec::new(),
+                attrs: Vec::new(),
+            };
+
+            wrap(tag, events, |events| inlines_to_events(inlines, events));
+        },
+        Block::CodeBlock { kind, code } => {
+            let kind = kind.to_pulldown_cmark();
+
+            wrap(Tag::CodeBlock(kind), events, |events| {
+                // FIXME: Is this the right event for raw codeblock content?
+                events.push(Event::Text(CowStr::from(code.as_str())))
+            })
+        },
+        Block::BlockQuote { kind, blocks } => {
+            wrap(Tag::BlockQuote(*kind), events, |events| {
+                for block in blocks {
+                    block_to_events(block, events)
+                }
+            })
+        },
+        Block::Table {
+            alignments,
+            headers,
+            rows,
+        } => {
+            // Structure of a table in Events:
+            //
+            // * Tag::Table
+            //   * Tag::TableHead
+            //     * Tag::TableCell...
+            //   * Tag::TableRow
+            //     * Tag::TableCell...
+
+            wrap(Tag::Table(alignments.clone()), events, |events| {
+                wrap(Tag::TableHead, events, |events| {
+                    for header_cell in headers {
+                        wrap(Tag::TableCell, events, |events| {
+                            inlines_to_events(header_cell, events);
+                        })
+                    }
+                });
+
+                for row in rows {
+                    wrap(Tag::TableRow, events, |events| {
+                        for row_cell in row {
+                            wrap(Tag::TableCell, events, |events| {
+                                inlines_to_events(row_cell, events);
+                            })
+                        }
+                    })
+                }
+            })
+        },
+        Block::Rule => events.push(Event::Rule),
+    }
+}
+
+fn wrap<'ast, F: FnOnce(&mut Vec<Event<'ast>>)>(
+    tag: Tag<'ast>,
+    events: &mut Vec<Event<'ast>>,
+    action: F,
+) {
+    let end = tag.to_end();
+
+    events.push(Event::Start(tag));
+    action(events);
+    events.push(Event::End(end));
+}
+
+fn inlines_to_events<'ast>(
+    inlines: &'ast Inlines,
+    events: &mut Vec<Event<'ast>>,
+) {
+    let Inlines(inlines) = inlines;
+
+    for inline in inlines {
+        match inline {
+            Inline::Text(text) => {
+                events.push(Event::Text(CowStr::from(text.as_str())));
+            },
+            Inline::Emphasis(inlines) => {
+                wrap(Tag::Emphasis, events, |events| {
+                    inlines_to_events(inlines, events)
+                })
+            },
+            Inline::Strong(inlines) => wrap(Tag::Strong, events, |events| {
+                inlines_to_events(inlines, events)
+            }),
+            Inline::Strikethrough(inlines) => {
+                wrap(Tag::Strikethrough, events, |events| {
+                    inlines_to_events(inlines, events)
+                })
+            },
+            Inline::Code(code) => {
+                events.push(Event::Code(CowStr::from(code.as_str())))
+            },
+            Inline::Link {
+                link_type,
+                dest_url,
+                title,
+                id,
+                content_text,
+            } => wrap(
+                Tag::Link {
+                    link_type: *link_type,
+                    dest_url: CowStr::from(dest_url.as_str()),
+                    // FIXME:
+                    //  Pass through this title; have a test that fails
+                    //  if this is empty.
+                    title: CowStr::from(title.as_str()),
+                    // FIXME: Passthrough this id
+                    // FIXME:
+                    //  Add test for the value of this field for every
+                    //  link type.
+                    id: CowStr::from(id.as_str()),
+                },
+                events,
+                |events| inlines_to_events(content_text, events),
+            ),
+            Inline::SoftBreak => events.push(Event::SoftBreak),
+            Inline::HardBreak => events.push(Event::HardBreak),
+        }
+    }
+}
